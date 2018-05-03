691e538996bf20273324d824d6449a93cd944962
Krystsina Kasko <Krystsina_Kasko@epam.com>
2018-05-03 18:41:43 +0300

    Adds JSON parser

---
+++ TestingTask1/src/main/java/JsonParser.java
@@ -0,0 +1,38 @@
+import org.json.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class JsonParser extends AbstractSource{
+
+    private ArrayList<String> Data;
+
+    @Override
+    public ArrayList<String> getData() {
+        return Data;
+    }
+
+    public void setData(ArrayList<String> data) {
+        Data = data;
+    }
+
+    public void parseJsonFile(String pathToFile) {
+        JSONParser jsonParser = new JSONParser();
+        ArrayList<String> listOfExpressions = new ArrayList<String>();
+
+        try {
+            JSONArray arrayOfData = (JSONArray) jsonParser.parse(new FileReader(pathToFile));
+            for (Object o : arrayOfData) {
+                listOfExpressions.add ((String) ((JSONObject) o).get("firstOperand") + ((JSONObject) o).get("operation") + ((JSONObject) o).get("secondOperand"));
+                setData(listOfExpressions);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
